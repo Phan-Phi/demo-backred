14fae9761baea47575829bdc544abdb89747b4e7
AnhPhi <phananhphi.pp@gmail.com>
2023-07-24 11:03:02 +0700

    update code 12

--- container/About/About.tsx
+++ container/About/About.tsx
@@ -38,7 +38,7 @@ export default function About(props: AboutPageProps) {
           <ImageRatio ratio="276/276" imageProps={{ src: logo }} />
         </Box> */}
 
-        <RenderContent data={histories} />
+        {/* <RenderContent data={histories} /> */}
 
         <Box sx={{ marginTop: "2.5rem" }}>
           {/* <Headline subTitle="THÀNH TÍCH ĐẠT ĐƯỢC" title="Chứng nhận & Chứng chỉ" /> */}
